86f22a607daaed3bcccf79c707b667342ddfe198
Matt Noone <matt.noone95@gmail.com>
2016-10-19 20:48:27 +1100

    CI setup

---
+++ Testing.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using SwinGameSDK;
+using MyGame;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class PatrolUnitTests
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [Test]
+        public void TestNumbers()
+        {
+            Point2D first = SwinGame.PointAt(10, 10);
+            Point2D second = SwinGame.PointAt(10, 10);
+
+            Assert.AreEqual(first.X, second.X);
+        }
+    }
+}
