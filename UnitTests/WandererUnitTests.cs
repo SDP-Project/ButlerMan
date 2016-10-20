4217adcc2f4ced8ba57fee596b8079a7ad192e65
Matt Noone <matt.noone95@gmail.com>
2016-10-20 12:19:01 +1100

    Re-added unit tests

---
+++ UnitTests/WandererUnitTests.cs
@@ -0,0 +1,53 @@
+﻿/*
+using NUnit.Framework;
+using System;
+using SwinGameSDK;
+using MyGame;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class WandererUnitTests
+    {
+// Currently removed pending refactoring
+//        Wanderer w;
+//        Tileset t;
+//        TileInteractor interactor;
+//
+//        [SetUp]
+//        public void Init()
+//        {
+//            w = new Wanderer();
+//            t = new Tileset();
+//
+//            for (int i = 0; i < 10; i++) {
+//                t.AddColumn ();
+//                t.AddRow ();
+//            }
+//
+//            interactor = new TileInteractor();
+//        }
+//
+//        [Test]
+//        public void InitialisationTest()
+//        {
+//            Assert.AreEqual(SwinGame.PointAt(0, 0), w.Pos);
+//        }
+//
+//        [Test]
+//        public void MovementTest()
+//        {
+//            w.TargetTile = TileInteractor.TileAt(w.Pos.X + 1, w.Pos.Y + 1);
+//
+//            w.Direction = Direction.SouthWest;
+//
+//            while (!w.TargetTile.IsAt (w.Pos, w.TargetTile.Pos))
+//            {
+//                w.Move();
+//            }
+//
+//            Assert.AreEqual(w.TargetTile.Pos, w.Pos);
+//        }
+    }
+}
+*/
