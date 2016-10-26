3b4f10e8071b95f4fb46e1ada47da42ee1da4d2f
Matt Noone <matt.noone95@gmail.com>
2016-10-26 11:52:58 +1100

    Added Player Breath status bar

--- Player.cs
+++ Player.cs
@@ -101,6 +101,15 @@ namespace MyGame
         public override void Render()
         {
             base.Render();
+
+            if (GameMain.gameState == GameState.InGame)
+            {
+                float pixelsPerBreath = (float)114 / (float)MAX_BREATH;
+                SwinGame.DrawText(pixelsPerBreath.ToString(), Color.Black, 100, 400);
+                SwinGame.DrawBitmap(SwinGame.BitmapNamed("StatusBar"), 50, 5);
+                SwinGame.FillRectangle(Color.Turquoise, 57, 7, pixelsPerBreath * CurrentBreath, 12);
+            }
+           
             SwinGame.DrawText(CurrentBreath.ToString(), Color.Black, 100, 100);
         }
 	}
